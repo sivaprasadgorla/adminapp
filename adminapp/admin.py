e704442263485212b98da33602606ee2697ff529
sivaprasadgorla <sivagorla007@gmail.com>
2024-04-27 08:25:35 +0530

    Initial commit

---
+++ adminapp/admin.py
@@ -0,0 +1,10 @@
+
+# Register your models here.
+from django.contrib import admin
+from .models import Product
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pid','pname','pcost','pmfdt','pexpdt']
+    list_filter = ['pname','pmfdt','pexpdt']
+    class meta:
+        model = Product
+admin.site.register(Product,ProductAdmin)
